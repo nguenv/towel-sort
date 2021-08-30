2364c397673c57345ce4c16f3528d8058a30d683
Your Name <nguenviktoria@gmail.com>
2021-08-30 20:30:59 +0300

    feat: add file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  let result = [];
+  if (matrix === 0 || matrix === undefined) {
   return [];
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j <matrix[i].length; j++) {
+      let columnInd = i % 2 === 0 ? j : (matrix[i].length - j - 1);
+      result.push((matrix[i][columnInd]));
+      
+    }
+  }
+    return result;
+  
 }
